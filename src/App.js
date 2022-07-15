9676da56c439a3ce7ecdb6bfdfc45ea40749c1d4
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-07-15 07:44:48 +0530

    amplify-added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route, Routes} from "react-router-dom";
 import routes from "./routes";
 import "./assets/theme.css"
+import awsconfig from "./aws-exports"
+import {Amplify} from "aws-amplify"
 import TopNavigation from "./components/TopNavigation/TopNavigation";
+
+Amplify.configure(awsconfig)
 
 function App() {
     return (
