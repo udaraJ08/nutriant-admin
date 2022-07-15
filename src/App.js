91d958013523cd2604f3e1777336f09a92c1ce78
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-07-15 08:41:00 +0530

    authguarded

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import routes from "./routes";
 import "./assets/theme.css"
 import awsconfig from "./aws-exports"
 import {Amplify} from "aws-amplify"
-import TopNavigation from "./components/TopNavigation/TopNavigation";
+import Login from "./views/Login/Login";
 
 Amplify.configure(awsconfig)
 
 function App() {
+
+    const navigate = useNavigate()
+
+    const routeGuard = () => {
+        const user = window.localStorage.getItem('user')
+
+        if (user) {
+            return routes.map((e, index) => {
+                return <Route key={index} path={e.path} element={e.component}/>
+            })
+        } else navigate('/')
+    }
+
     return (
         <div className="App">
             <div className="p-3">
                 <Routes>
-                    {routes.map((e, index) => {
-                        return <Route key={index} path={e.path} element={e.component}/>
-                    })}
+                    <Route path={'/'} element={<Login/>}/>
+                    <Route path={'*'} element={<Login/>}/>
+                    {routeGuard()}
                 </Routes>
             </div>
         </div>
